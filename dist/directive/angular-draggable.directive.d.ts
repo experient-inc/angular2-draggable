f5d4839fb6549678624c50176459579b8ddf7d52
Zeender, Robert <robert.zeender@experient-inc.com>
2018-01-23 11:34:22 -0500

    remove 'dist' from .ignore

---
+++ dist/directive/angular-draggable.directive.d.ts
@@ -0,0 +1,32 @@
+import { ElementRef, Renderer, OnInit, EventEmitter, OnDestroy } from '@angular/core';
+export declare class AngularDraggableDirective implements OnInit, OnDestroy {
+    private el;
+    private renderer;
+    private allowDrag;
+    private moving;
+    private orignal;
+    private oldTrans;
+    private tempTrans;
+    private oldZIndex;
+    private oldPosition;
+    private _resetSub;
+    started: EventEmitter<any>;
+    stopped: EventEmitter<any>;
+    handle: HTMLElement;
+    reset: EventEmitter<any>;
+    ngDraggable: any;
+    constructor(el: ElementRef, renderer: Renderer);
+    ngOnInit(): void;
+    ngOnDestroy(): void;
+    private getPosition(x, y);
+    private moveTo(x, y);
+    private pickUp();
+    private putBack();
+    onMouseDown(event: any): void;
+    onMouseUp(): void;
+    onMouseLeave(): void;
+    onMouseMove(event: any): void;
+    onTouchEnd(): void;
+    onTouchStart(event: any): void;
+    onTouchMove(event: any): void;
+}
